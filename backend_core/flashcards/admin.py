64166ee61f4212208a2aa7720df2a2bafac11c05
t.kajdrowicz <t.kajdrowicz@aotm.gov.pl>
2023-02-08 21:49:10 +0100

    flashcards

--- backend_core/flashcards/admin.py
+++ backend_core/flashcards/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.DifficultyLevel)
+admin.site.register(models.Tag)
+admin.site.register(models.Deck)
+admin.site.register(models.Flashcard)
+
